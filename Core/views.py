d1d9b010378daf72d3b68090f1118c2ceef0db6d
salah jabri <gampodark120@gmail.com>
2023-05-27 17:30:01 +0100

    verion 1.20

--- Core/views.py
+++ Core/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from Product.models import Category,MotorCycle
 # Create your views here.
 def index(request):
-    return render(request,'core/index.html',{})
+    motos = MotorCycle.objects.filter(is_solder=False)
+    cat = Category.objects.all()
+
+    return render(request,'core/index.html',{
+        'categories':cat,
+        'motos':motos
+    })
 
 def contact(request):
     return render(request,"core/contact.html",{})
